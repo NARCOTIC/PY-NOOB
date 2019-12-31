cb3151691a764fd916def496204405ec003453ed
NARCOTIC <58453114+NARCOTIC@users.noreply.github.com>
2019-12-31 10:51:01 +0000

    Variables, Print, Basic Math & If Conditions

--- Lesson-001.py
+++ Lesson-001.py
@@ -3,12 +3,14 @@ x = 0                                                         # Assigning a valu
 y = 0                                                         # Assigning a value to a Variable
 z = 0                                                         # Assigning a value to a Variable
 n = 0                                                         # Assigning a value to a Variable
+c = 0                                                         # Assigning a value to a Variable
 
 print("BASICS OF PYTHON | GITHUB/NARCOTIC | LESSON-001")      # Print to Screen
 print()                                                       # Print an Empty Line
 
 x = int(input("Enter x Value : "))                            # Input a value to a Integer Variable
 y = int(input("Enter y Value : "))                            # Input a value to a Integer Variable
+c = int(input("Enter Common Factor : "))                            # Input a value to a Integer Variable
 n = input("Enter Your Name : ")                               # Input a String
 
 zA = x + y                                                    # Addition
@@ -27,27 +29,27 @@ print()
 
 if (zA % 2) == 0:                                             # If the Remainder of zA is 0 after deviding by 2
   print("Sum of x and y is Even")                             # In other words: if zA is Even
-    
-  if zA < 0:                                                  # If sum of x and y is less than 0
-    print("Sum of x and y is a Negative Number")
-    
-  elif zA > 0:                                                # Else If sum of x and y is greater than 0
-    print("Sum of x and y is a Positive Number")
-    
-  else:                                                       # Else sum of x and y is = 0
-    print("Sum of x and y is 0")
 
 else:                                                         # If the Remainder of zA is 1 after deviding by 2
   print("Sum of x and y is Odd")                              # In other words: if zA is Odd
+    
+if zA < 0:                                                    # If sum of x and y is less than 0
+  print("Sum of x and y is a Negative Number")
+    
+elif zA > 0:                                                  # Else If sum of x and y is greater than 0
+  print("Sum of x and y is a Positive Number")
+    
+else:                                                         # Else sum of x and y is = 0
+  print("Sum of x and y is 0")
 
-  if zA < 0:                                                  # If sum of x and y is less than 0
-    print("Sum of x and y is a Negative Number")
-    
-  elif zA > 0:                                                # Else If sum of x and y is greater than 0
-    print("Sum of x and y is a Positive Number")
-    
-  else:                                                       # Else sum of x and y is = 0
-    print("Sum of x and y is 0")
+if x % c == 0 and y % c == 0:                                 # Using 'and' Logical Operation with if Condition
+  print("x and y both have common factor:", c)
+
+elif x % c == 0 or y % c == 0:                                # Using 'and' Logical Operation with if Condition
+  print("x or y has common factor:", c)
+
+else:                                                         # Else
+  print("x or y does not have common factor:", c)
 
 print()
 print("THAT'S IT FOR FIRST LESSON. TAKE A LOOK AT THE SECOND ONE")
