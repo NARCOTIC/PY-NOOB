1cbbbec10e80e3b06e078450d8c2b395a0e42a23
NARCOTIC <58453114+NARCOTIC@users.noreply.github.com>
2020-01-01 07:25:46 +0000

    While Loops, String Elements, Function: len()

--- Lesson002.py
+++ Lesson002.py
@@ -1,25 +1,21 @@
 i = 0                                                         # Assigning a value to a Variable
-x = 0                                                         # Assigning a value to a Variable
 
 print("BASICS OF PYTHON | GITHUB/NARCOTIC | LESSON-002")      # Print to Screen
 print()                                                       # Print an Empty Line
 
 strng = input("Enter a String : ")                            # Input a String
-x = int(input("Print first __ characters : "))                # Input a value to a Integer Variable
+first_chars = int(input("Print first __ characters : "))      # Input a value to a Integer Variable
 print()                                                       # Print an Empty Line
 
-if x <= len(strng):                                           # Function: len() returns the length of a String
+if first_chars <= len(strng):                                 # Function: len() returns the length of a String
                                                               # If the length of String is less than or equal to x
-    
-  while i < x:                                                # While Loop
+  while i < first_chars:                                      # While Loop
     print(strng[i])                                           # Print the String as an array elements
-   i=i+1                                                      # Increment i by 1
-    
-#  print(strng[:x])                                           # This one line and the previous 3 lines do the same thing
+    i=i+1                                                     # Increment i by 1
 
 else:                                                         # If the length of String is greater than x
   print("String contains only ", len(strng), " items.")       # Print to Screen
-  print("Cannot output ", x, " items.")                       # Print to Screen
+  print("Cannot output ", first_chars, " items.")             # Print to Screen
 
 print()
 print("THAT'S IT FOR SECOND LESSON. TAKE A LOOK AT THE THIRD ONE")
